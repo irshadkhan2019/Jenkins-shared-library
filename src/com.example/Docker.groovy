bde156476238c34af509ebfa06f18c8856971f4b
Irshad Khan <khanirshad2019@gmail.com>
2024-06-04 11:04:26 +0530

    Using class

---
+++ src/com.example/Docker.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Sericalizable{
+    def script //used to stores all variable available in Jenkinsfile
+    
+    Docker(script){
+        this.script=script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo 'Building Image'
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub',passwordVariable: 'PASS', usernameVariable: 'USER')]){
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            script.sh "docker push $imageName"
+            }    
+        }
+    }
